793edbc7a255d7427058dccfcfcac684dc1178be
Yu Jui Wang <b11611009@ntu.edu.tw>
2026-06-08 18:19:03 +0800

    change to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
@@ -8,22 +8,22 @@ import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
-    <BrowserRouter basename="/rex-portfolio">
+    <HashRouter>
       <div className="app">
         <Header />
 
         <main className="site">
           <Routes>
-            <Route path="/" element={<Navigate to="/resume" replace />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/portfolio" element={<Portfolio />} />
+            <Route index element={<Navigate to="/resume" replace />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="portfolio" element={<Portfolio />} />
             <Route path="*" element={<Navigate to="/resume" replace />} />
           </Routes>
         </main>
 
         <ContactFooter />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
